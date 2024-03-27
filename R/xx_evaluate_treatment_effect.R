1d2199a777db64203754bd9a9fd17b4b596c3750
Vinayak Anand-Kumar <v@Vinayaks-Mac-mini.local>
2024-03-27 19:19:07 +0100

    instructions added for calculating rmse of treatment effects and creating plots

---
+++ R/xx_evaluate_treatment_effect.R
@@ -0,0 +1,7 @@
+
+
+# get a vector of parameter estimates
+
+# create a vector of true parameter values
+
+# compute the RMSE between estimate and true values
---
+++ R/xx_visualise_estimates.R
@@ -0,0 +1,5 @@
+
+
+# create a data frame with parameter estimates and confidence intervals
+
+# plot data farme based on ggplot code in plot_estimates.R
