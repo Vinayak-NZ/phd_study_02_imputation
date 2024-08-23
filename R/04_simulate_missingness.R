d3e6e95661387c450466d2ac3bcf7d5e515d0ccf
Vinayak Anand-Kumar <v@Vinayaks-Mac-mini.local>
2024-08-23 11:43:53 +0200

    finished simulations

--- R/04_simulate_missingness.R
+++ R/04_simulate_missingness.R
@@ -6,7 +6,7 @@ sc_missing_mcar_20 <- simulate_missingness_bulk(data = synthetic_data_subset,
                                              var = "selfcare", 
                                              proportion = 0.2,
                                              miss_mech = "MCAR",
-                                             iterations = 5)
+                                             iterations = 100)
 
 #MCAR-30
 
@@ -14,7 +14,7 @@ sc_missing_mcar_30 <- simulate_missingness_bulk(data = synthetic_data_subset,
                                              var = "selfcare", 
                                              proportion = 0.3,
                                              miss_mech = "MCAR",
-                                             iterations = 5)
+                                             iterations = 100)
 
 #MCAR-40
 
@@ -22,7 +22,7 @@ sc_missing_mcar_40 <- simulate_missingness_bulk(data = synthetic_data_subset,
                                                 var = "selfcare", 
                                                 proportion = 0.4,
                                                 miss_mech = "MCAR",
-                                                iterations = 5)
+                                                iterations = 100)
 
 #MAR-20
 
@@ -30,7 +30,7 @@ sc_missing_mar_20 <- simulate_missingness_bulk(data = synthetic_data_subset,
                                                 var = "selfcare", 
                                                 proportion = 0.2,
                                                 miss_mech = "MAR",
-                                                iterations = 5)
+                                                iterations = 100)
 
 #MAR-30
 
@@ -38,7 +38,7 @@ sc_missing_mar_30 <- simulate_missingness_bulk(data = synthetic_data_subset,
                                                 var = "selfcare", 
                                                 proportion = 0.3,
                                                 miss_mech = "MAR",
-                                                iterations = 5)
+                                                iterations = 100)
 
 #MAR-40
 
@@ -46,7 +46,7 @@ sc_missing_mar_40 <- simulate_missingness_bulk(data = synthetic_data_subset,
                                                 var = "selfcare", 
                                                 proportion = 0.4,
                                                 miss_mech = "MAR",
-                                                iterations = 5)
+                                                iterations = 100)
 
 #MNAR-20
 
@@ -54,7 +54,7 @@ sc_missing_mnar_20 <- simulate_missingness_bulk(data = synthetic_data_subset,
                                                 var = "selfcare", 
                                                 proportion = 0.2,
                                                 miss_mech = "MNAR",
-                                                iterations = 5)
+                                                iterations = 100)
 
 #MNAR-30
 
@@ -62,7 +62,7 @@ sc_missing_mnar_30 <- simulate_missingness_bulk(data = synthetic_data_subset,
                                                 var = "selfcare", 
                                                 proportion = 0.3,
                                                 miss_mech = "MNAR",
-                                                iterations = 5)
+                                                iterations = 100)
 
 #MNAR-40
 
@@ -70,7 +70,7 @@ sc_missing_mnar_40 <- simulate_missingness_bulk(data = synthetic_data_subset,
                                                 var = "selfcare", 
                                                 proportion = 0.4,
                                                 miss_mech = "MNAR",
-                                                iterations = 5)
+                                                iterations = 100)
 
 ## ---- physical-activity-missingness
 
@@ -80,7 +80,7 @@ pa_missing_mcar_20 <- simulate_missingness_bulk(data = synthetic_data_subset,
                                                 var = "physical_activity", 
                                                 proportion = 0.2,
                                                 miss_mech = "MCAR",
-                                                iterations = 5)
+                                                iterations = 100)
 
 #MCAR-30
 
@@ -88,7 +88,7 @@ pa_missing_mcar_30 <- simulate_missingness_bulk(data = synthetic_data_subset,
                                                 var = "physical_activity", 
                                                 proportion = 0.3,
                                                 miss_mech = "MCAR",
-                                                iterations = 5)
+                                                iterations = 100)
 
 #MCAR-40
 
@@ -96,7 +96,7 @@ pa_missing_mcar_40 <- simulate_missingness_bulk(data = synthetic_data_subset,
                                                 var = "physical_activity", 
                                                 proportion = 0.4,
                                                 miss_mech = "MCAR",
-                                                iterations = 5)
+                                                iterations = 100)
 
 #MAR-20
 
@@ -104,7 +104,7 @@ pa_missing_mar_20 <- simulate_missingness_bulk(data = synthetic_data_subset,
                                                 var = "physical_activity", 
                                                 proportion = 0.2,
                                                 miss_mech = "MAR",
-                                                iterations = 5)
+                                                iterations = 100)
 
 #MAR-30
 
@@ -112,7 +112,7 @@ pa_missing_mar_30 <- simulate_missingness_bulk(data = synthetic_data_subset,
                                                 var = "physical_activity", 
                                                 proportion = 0.3,
                                                 miss_mech = "MAR",
-                                                iterations = 5)
+                                                iterations = 100)
 
 #MAR-40
 
@@ -120,7 +120,7 @@ pa_missing_mar_40 <- simulate_missingness_bulk(data = synthetic_data_subset,
                                                 var = "physical_activity", 
                                                 proportion = 0.4,
                                                 miss_mech = "MAR",
-                                                iterations = 5)
+                                                iterations = 100)
 
 #MNAR-20
 
@@ -128,7 +128,7 @@ pa_missing_mnar_20 <- simulate_missingness_bulk(data = synthetic_data_subset,
                                                var = "physical_activity", 
                                                proportion = 0.2,
                                                miss_mech = "MNAR",
-                                               iterations = 5)
+                                               iterations = 100)
 
 #MNAR-30
 
@@ -136,7 +136,7 @@ pa_missing_mnar_30 <- simulate_missingness_bulk(data = synthetic_data_subset,
                                                var = "physical_activity", 
                                                proportion = 0.3,
                                                miss_mech = "MNAR",
-                                               iterations = 5)
+                                               iterations = 100)
 
 #MNAR-40
 
@@ -144,4 +144,4 @@ pa_missing_mnar_40 <- simulate_missingness_bulk(data = synthetic_data_subset,
                                                var = "physical_activity", 
                                                proportion = 0.4,
                                                miss_mech = "MNAR",
-                                               iterations = 5)
+                                               iterations = 100)
